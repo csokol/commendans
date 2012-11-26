76ba8b28c9475ac860f1d286ec9398b81b82b302
Francisco Sokol <chico.sokol@gmail.com>
2012-11-26 20:01:45 -0200

    commenting import method

--- src/main/java/br/ime/usp/commendans/controller/IndexController.java
+++ src/main/java/br/ime/usp/commendans/controller/IndexController.java
@@ -1,13 +1,11 @@
 package br.ime.usp.commendans.controller;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.hibernate.Session;
 
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.view.Results;
-import br.ime.usp.commendans.infra.DataImporter;
 import br.ime.usp.commendans.recommender.RecommenderCreator;
 
 
@@ -30,12 +28,12 @@ public class IndexController {
     
     @Get("/import/askjdhakjshdakjshd")
     public void importData() {
-        DataImporter dataImporter = new DataImporter(session);
-        dataImporter.importData("/orders.csv");
-        recommenderCreator.create();
-        String name = "Casa do Código";
-        String accessKey = "123" + name;
-        accessKey = DigestUtils.sha256Hex(accessKey);
-        result.use(Results.http()).body("<html><body>finished persisting, key: "+accessKey+"</body></html>");
+//        DataImporter dataImporter = new DataImporter(session);
+//        dataImporter.importData("/orders.csv");
+//        recommenderCreator.create();
+//        String name = "Casa do Código";
+//        String accessKey = "123" + name;
+//        accessKey = DigestUtils.sha256Hex(accessKey);
+        result.use(Results.http()).body("<html><body>get outta here</body></html>");
     }
 }
